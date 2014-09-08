021ca590e10c26599ef61d29705441e23f654648
Mark Wienk <mark.wienk@core9.io>
2014-09-08 22:24:24 +0200

    Order parsed from Map

--- src/impl/java/io/core9/module/commerce/ogone/OgoneDataHandlerImpl.java
+++ src/impl/java/io/core9/module/commerce/ogone/OgoneDataHandlerImpl.java
@@ -1,7 +1,9 @@
 package io.core9.module.commerce.ogone;
 
 import io.core9.commerce.checkout.Order;
+import io.core9.commerce.checkout.OrderImpl;
 import io.core9.module.auth.AuthenticationPlugin;
+import io.core9.plugin.database.repository.DataUtils;
 import io.core9.plugin.server.request.Request;
 import io.core9.plugin.widgets.datahandler.DataHandler;
 import io.core9.plugin.widgets.datahandler.DataHandlerFactoryConfig;
@@ -45,10 +47,16 @@ public class OgoneDataHandlerImpl implements OgoneDataHandler {
 		final OgoneDataHandlerConfig config = (OgoneDataHandlerConfig) options; 
 		return new DataHandler<OgoneDataHandlerConfig>() {
 
+			@SuppressWarnings("unchecked")
 			@Override
 			public Map<String, Object> handle(Request req) {
 				Map<String, Object> result = new HashMap<String, Object>();
-				Order order = (Order) auth.getUser(req).getSession().getAttribute("order");
+				Object tmp = auth.getUser(req).getSession().getAttribute("order");
+				OrderImpl order = null;
+				if(tmp instanceof Map) {
+					order = DataUtils.toObject((Map<String,Object>) tmp, OrderImpl.class);
+				}
+				
 				if(order == null) {
 					req.getResponse().sendRedirect(301, "/");
 					return result;
