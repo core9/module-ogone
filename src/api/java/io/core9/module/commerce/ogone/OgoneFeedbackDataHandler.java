42d18dbb74f57c5c16a45a7fa07e4bda28605b6f
Mark Wienk <mark.wienk@core9.io>
2014-12-02 12:45:55 +0100

    removed feedback datahandler

--- src/api/java/io/core9/module/commerce/ogone/OgoneFeedbackDataHandler.java
+++
@@ -1,8 +0,0 @@
-package io.core9.module.commerce.ogone;
-
-import io.core9.core.plugin.Core9Plugin;
-import io.core9.plugin.widgets.datahandler.DataHandlerFactory;
-
-public interface OgoneFeedbackDataHandler<T extends OgoneFeedbackDataHandlerConfig> extends DataHandlerFactory<T>, Core9Plugin {
-
-}
